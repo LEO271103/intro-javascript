98575e90602f8c0b105228f65841a686294bf95d
Leonardo Saul <2122300446@soy.utj.edu.mx>
2023-09-20 18:35:15 -0600

    Practica 1 Terminada

---
+++ src/const.js
@@ -0,0 +1,11 @@
+const nombre='leonardo', apellido='sanchez';
+const nombreCompleto=nombre+" "+ apellido;
+
+console.log(nombreCompleto);
+
+const nombreCompleto2= ` hola  
+
+mundo `;
+
+console.log(nombreCompleto2);
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const nombre= 'leo', apellido = 'avila sanchez';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let dado= 5;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+if (true){
+  let dado=6;
+  const nombre= 'jaja', apellido = 'avihez';
+
+  console.log(dado, nombre, apellido);
+} 
+
+
+console.log("hola mundo" , nombre, apellido, dado);
